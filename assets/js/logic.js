89c1107bbf51055ec060d9572961ba2ae0d2afd8
Bibbleboop <nicjah@hotmail.com>
2023-04-02 06:06:44 +0100

    clean up

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,9 @@
-***** part of code from nirmeet day 2 week 7  also see pod7 challenge logic vid
-
 // Define Variables  - can also use getElementById
-const startBtn = document.querySelector(".start");
+const startBtn = document.querySelector("#start-button");
 const startSection = document.querySelector(".start-screen");
 const questionTitle = document.querySelector(".question-title");
 const choices = document.querySelector(".choices");
+const submitBtn = document.querySelector("#submit-button");
 const questionsDiv = document.querySelector(".questions");
 const highScores = document.querySelector(".high-scores");
 const clearScores = document.querySelector(".clear-scores");
@@ -12,10 +11,11 @@ const clearScores = document.querySelector(".clear-scores");
 let index = 0;
 let time = 0;
 let interval = 0;
+let timeleft = 0;
 let timer = document.querySelector(".timer");
 let wrapper = document.querySelector(".wrapper");
 
-let questionNumber = 0;
+
 
 function startQuiz(event) {
     startSection.style.display = 'none';
@@ -24,15 +24,8 @@ function startQuiz(event) {
 
 
 }
+// Event listen er for staring quiz and submitting choices
+startBtn.addEventListener('click', start-button);
+submitBtn.addEventListener('click, submit-button');
 
-startBtn.addEventListener('click', startQuiz);
 
-
-// Click through the 'Start Quiz' button
-// start.addEventListener("start", () => { 
-//     //check answer
-//     if choiceNo === QUIZ[index].answer) {
-
-//     }
-//     console.log(startQuiz)
-// });
