cd965954676a3faa806d338b63e8a05d6c66b7b0
Bibbleboop <nicjah@hotmail.com>
2023-04-02 06:25:22 +0100

    highscore first commit

---
+++ assets/js/scores.js
@@ -0,0 +1,11 @@
+const highScores = document.querySelector("#highscores");
+const clearScores = document.querySelector("#clear");
+
+Object.keys(localStorage).forEach(function (key) {
+    console.log(
+highScores.appendChild;
+
+startBtn.addEventListener('click', function() {
+    localStorage.clear();
+    highScores.innerHTML = " ";
+});
